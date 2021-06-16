b0790c8b36bf5ab5b51c60e700b6aa4cacdef517
Matthias Bader <matthias.bader@roche.com>
2021-06-16 10:00:43 +0200

    Rework how schema resolving works.

--- JsonValidator/Program.cs
+++ JsonValidator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
@@ -38,111 +39,128 @@ namespace JsonValidator
         //private static readonly string RelPathToTypesDirectory = null;
         //static IEnumerable<string> JsonFileNames = Directory.GetFiles(Path.Combine(BaseDir, "examples"), @"*.json", SearchOption.AllDirectories);
 
-        //// Prepared settings: (5) Data Upload
+        // Prepared settings: (5) Data Upload
         //static readonly string BaseDir = @"C:\Code\GitLab\dm-data-upload\";
-        //private static readonly string RelPathToTypesDirectory = @"x800\dm";
-        //static IEnumerable<string> JsonFileNames = Directory.GetFiles(Path.Combine(BaseDir, "examples"), @"*.json", SearchOption.AllDirectories);
         //private static IEnumerable<string> JsonFileNames = new string[]
         //{
-        //    @"C:\Code\GitLab\dm-data-upload\Example\Package-BBA48610-5427-4A27-95FE-3DC50AA1EDF4.metadata.json",
-        //    @"C:\Code\GitLab\dm-data-upload\Example\Bundle 1\Settings_2019_04_18_15_08_32_500.json",
-        //    @"C:\Code\GitLab\dm-data-upload\Example\Bundle 2\Inventory_2019_04_18_15_11_00_018.json",
+        //    @"C:\Code\GitLab\dm-data-upload\examples\compositions\EventsComposition_2019_04_18_16_52_01_343.json",
+        //    @"C:\Code\GitLab\dm-data-upload\examples\compositions\InventoryComposition_2019_04_18_15_11_00_018.json",
+        //    @"C:\Code\GitLab\dm-data-upload\examples\compositions\ResultsComposition_2019_04_18_15_09_27_321.json",
+        //    @"C:\Code\GitLab\dm-data-upload\examples\packages\Package-BBA48610-5427-4A27-95FE-3DC50AA1EDF4.metadata.json",
         //};
 
-        // Prepared settings: (6) Calculator Interface
-        private static string BaseDir = @"C:\Code\GitLab\calculator-interface";
-        private static readonly string RelPathToTypesDirectory = @"X800\types"; 
-        private static IEnumerable<string> JsonFileNames = Directory.GetFiles(Path.Combine(BaseDir, "examples"), @"*.json", SearchOption.AllDirectories);
+
+        // Prepared settings: (6) Data Export
+        static readonly string BaseDir = @"C:\Code\GitLab\dm-data-export\json\";
+        private static readonly string RelPathToTypesDirectory = @"X800";
+        static IEnumerable<string> JsonFileNames = Directory.GetFiles(Path.Combine(BaseDir, "Examples"), @"*.json", SearchOption.AllDirectories);
+
+        //// Prepared settings: (7) Calculator Interface
+        //private static string BaseDir = @"C:\Code\GitLab\calculator-interface";
+        //private static readonly string RelPathToTypesDirectory = @"X800\types";
+        //private static IEnumerable<string> JsonFileNames = Directory.GetFiles(Path.Combine(BaseDir, "examples"), @"*.json", SearchOption.AllDirectories);
+
+
 
         private static readonly string SchemaDir = Path.Combine(BaseDir, @"schema");
         private static readonly Uri BaseUri = new Uri(@"http://roche.com/rmd/");
 
+
+
+
         static void Main()
         {
-            // create a resolver with all type-schemas loaded.
+            IList<string> errors = new List<string>();
+
+            // Get a list of all schemas that are in scope.
+            IDictionary<Uri, string> schemaDict = GetAllSchema(SchemaDir);
+            Console.WriteLine("The following schemas were found:");
+            foreach (Uri uri in schemaDict.Keys)
+            {
+                Console.WriteLine($"  {uri}");
+            }
+
+            // Preload a resolver with all these schemas.
             JSchemaPreloadedResolver resolver = new JSchemaPreloadedResolver();
-            if (!string.IsNullOrEmpty(RelPathToTypesDirectory))
+            foreach (KeyValuePair<Uri, string> schema in schemaDict)
             {
-                AddTypesToResolver(resolver, Path.Combine(SchemaDir, RelPathToTypesDirectory));
+                JObject jObject = LoadJsonFile(schema.Value);
+                resolver.Add(schema.Key, jObject.ToString());
             }
 
-            Console.WriteLine("SchemaResolver was initialized with the following types:");
-            foreach (Uri uri in resolver.PreloadedUris)
-            {
-                Console.WriteLine("  {0}", uri);
-            }
-
-            // load all examples and validate them.
+            // Validate all json-files.
             Console.WriteLine();
             Console.WriteLine("Validating examples");
-
-            IList<string> invalidFileNames = new List<string>();
-            foreach (string jsonMessageFileName in JsonFileNames)
+            foreach (string jsonFileName in JsonFileNames)
             {
-                JObject jsonMessage = LoadJsonFile(jsonMessageFileName);
-                Uri referencedMessageSchema = GetReferencedSchema(jsonMessage);
-
-                JSchema messageSchema = LoadMessageSchema(referencedMessageSchema, resolver);
-                bool isValid = jsonMessage.IsValid(messageSchema, out IList<string> errors);
-
-                Console.WriteLine("  {0} :  {1}", isValid ? "Valid" : "Invalid", jsonMessageFileName);
-                if (errors != null)
+                if (!File.Exists(jsonFileName))
                 {
-                    foreach (string error in errors)
+                    errors.Add($"[FileNotFound] '{jsonFileName}'");
+                    continue;
+                }
+
+                JObject jsonObject;
+                try
+                {
+                    jsonObject = LoadJsonFile(jsonFileName);
+                }
+                catch
+                {
+                    errors.Add($"[InvalidJsonFile] '{jsonFileName}'");
+                    continue;
+                }
+
+                // Each json-file has a 'schema' property that indicates the schema it is associated with ('referencedSchema'). 
+                string referencedSchema = jsonObject.Value<string>("schema") ?? null;
+                if (referencedSchema == null)
+                {
+                    errors.Add($"[SchemaNotReferenced] File '{jsonFileName}' does not contain a schema-reference ('schema'-property).");
+                    continue;
+                }
+                Uri referencedSchemaUri = new Uri(referencedSchema);
+
+                // Get the schema content.
+                string referencedSchemaFile;
+                if (!schemaDict.TryGetValue(referencedSchemaUri, out referencedSchemaFile))
+                {
+                    errors.Add($"[SchemaNotFound] Unable to find schema referenced in file '{jsonFileName}'.");
+                    continue;
+                }
+
+                // Validate file against schema.
+                JSchema schema = LoadSchema(resolver, referencedSchemaFile);
+                bool isValid = jsonObject.IsValid(schema, out IList<string> validationErrors);
+
+                Console.WriteLine("  {0} :  {1}", isValid ? "Valid" : "Invalid", jsonFileName);
+                if (validationErrors != null)
+                {
+                    foreach (string validationError in validationErrors)
                     {
-                        Console.WriteLine(error);
+                        Console.WriteLine(validationError);
                     }
                 }
 
                 if (!isValid)
                 {
-                    invalidFileNames.Add(jsonMessageFileName);
+                    errors.Add($"[ValidationError] Filename: '{jsonFileName}'");
                 }
             }
 
             Console.WriteLine();
-            if (invalidFileNames.Count == 0)
+            if (errors.Count == 0)
             {
-                Console.WriteLine("No invalid examples found!");
+                Console.WriteLine("No errors found!");
             }
             else
             {
-                Console.WriteLine("{0} invalid files found:", invalidFileNames.Count);
-                foreach (string invalidFileName in invalidFileNames)
+                Console.WriteLine("{0} errors found:", errors.Count);
+                foreach (string invalidFileName in errors)
                 {
                     Console.WriteLine(" - {0}", invalidFileName);
                 }
             }
 
             Console.ReadKey();
-        }
-
-        private static Uri GetReferencedSchema(JObject jsonObject)
-        {
-            var schemaRef = jsonObject.Properties().FirstOrDefault(p => p.Name.Equals("schema"));
-            if (schemaRef == null)
-            {
-                return null;
-            }
-
-            return new Uri(schemaRef.Value.ToString());
-        }
-
-        private static JSchema LoadMessageSchema(Uri messageUri, JSchemaResolver resolver)
-        {
-            if (!BaseUri.IsBaseOf(messageUri))
-            {
-                throw new Exception("Invalid reference URI.");
-            }
-
-            Uri relativeUri = BaseUri.MakeRelativeUri(messageUri);
-            string messageSchemaFileName = Path.Combine(SchemaDir, relativeUri.ToString());
-            if (!File.Exists(messageSchemaFileName))
-            {
-                throw new Exception("The URI doesn't refer to an existing message-schema.");
-            }
-
-            return LoadSchema(resolver, messageSchemaFileName);
         }
 
         private static JObject LoadJsonFile(string fileName)
@@ -166,26 +184,50 @@ namespace JsonValidator
                 }
             }
         }
-
-        private static void AddTypesToResolver(JSchemaPreloadedResolver resolver, string typesDirectory)
+         
+        private static IDictionary<Uri, string> GetAllSchema(string typesDirectory)
         {
-            JSchema rootSchema = ReadJsonSchemaDraft04();
+            var schemaFiles = new Dictionary<Uri, string>();
 
-            IEnumerable<string> schemaFileNames = Directory.GetFiles(typesDirectory, @"*.json", SearchOption.AllDirectories);
-            foreach (string schemaFileName in schemaFileNames)
+            JSchema json04Schema = ReadJsonSchemaDraft04();
+
+            IEnumerable<string> fileNames = Directory.GetFiles(typesDirectory, @"*.json", SearchOption.AllDirectories);
+            foreach (string fileName in fileNames)
             {
-                JObject schema = LoadJsonFile(schemaFileName);
-                bool isValid = schema.IsValid(rootSchema);
-                if (!isValid)
+                JObject jsonObject = LoadJsonFile(fileName);
+
+                // X800 schema files have "$schema" property: Identifies the JSON schema version
+                string schema = jsonObject.Value<string>("$schema") ?? null;
+                if (string.IsNullOrEmpty(schema))
                 {
-                    throw new InvalidDataException($"Schema {schemaFileName} is invalid.");
+                    continue;
                 }
 
-                string url = schema.Properties().First(p => p.Name.Equals("id")).Value.ToString();
-                resolver.Add(new Uri(url), schema.ToString());
-            
+                bool isValid = jsonObject.IsValid(json04Schema);
+                if (!isValid)
+                {
+                    throw new InvalidDataException($"Schema in file '{fileName}' is invalid (not schema compliant).");
+                }
+
+                // X800 schema files have an "id"-property: A unique identifier of the schema (a URI)
+                string id = jsonObject.Value<string>("id") ?? null;
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new InvalidDataException($"Schema in file '{fileName}' lacks the 'id' property.");
+                }
+
+                Uri uri = new Uri(id);
+                if (schemaFiles.ContainsKey(uri))
+                {
+                    throw new InvalidDataException($"There exists multiple schema-files with id '{uri.ToString()}'.");
+                }
+
+                schemaFiles.Add(uri, fileName);
             }
+
+            return schemaFiles;
         }
+
 
         private static JSchema ReadJsonSchemaDraft04()
         {
